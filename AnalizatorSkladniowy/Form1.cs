691e4988fb6f607534c04c8b29ab62989cd942c6
solenski <solenski@outlook.com>
2017-12-16 00:51:18 +0100

    first and last

---
+++ AnalizatorSkladniowy/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalizatorSkladniowy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this,
+                new SyntaxAnalyzer().IsValidExpression(this.textBox1.Text.Replace(" ", string.Empty)) ? "Expression valid" : "Expression invalid",
+                "Result", MessageBoxButtons.OK);
+        }
+    }
+}
---
+++ AnalizatorSkladniowy/ISyntaxAnalyser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AnalizatorSkladniowy
+{
+    public interface ISyntaxAnalyser
+    {
+        Dictionary<string, IEnumerable<char>> terminalSymbols { get; }
+        bool IsValidExpression(string s);
+    }
+}
