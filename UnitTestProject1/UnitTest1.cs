ed3239ec2560edfe3986805554df1f6a69dbca15
solenski <solenski@outlook.com>
2018-01-03 23:04:45 +0100

    not fucked up version

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using WindowsFormsApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private SimpleEquationSyntaxAnalyser sesa => new SimpleEquationSyntaxAnalyser();
+
+        public string[] OKStrings = new string[] {
+            "0*0",
+            "0+8",
+            "2+4+5-3-2-1",
+            "2+4",
+            "3+3+3+1",
+            "-3+2-1",
+            "9+3+4*3/2",
+            "0+2",
+            "-0+3",
+            "-0 + 2",
+            "0-5-0+0-0+3-0+3+3+3",
+            "130+32",
+            "111+ 33+1 2121",
+            "12332+33+12121 "};
+        public string[] NOKStrings = new string[] {
+            "a+c",
+            "++1-2",
+            "00+3",
+            "0+",
+            "---2+4+5-3-2-1",
+            "-2++++4",
+            "/3++3+3+13",
+            "--3+2-1",
+            "9+/3+4*3/2",
+            "02+2",
+            "000+2"
+        };
+
+
+        [TestMethod]
+        public void TestOK()
+        {
+            foreach (var tcase in OKStrings)
+            {
+                Assert.IsTrue(this.sesa.Validate(tcase), tcase);
+            }
+        }
+
+        [TestMethod]
+        public void TestNOK()
+        {
+            foreach (var tcase in NOKStrings)
+            {
+                Assert.IsFalse(this.sesa.Validate(tcase), tcase);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/SimpleEquationSyntaxAnalyser.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class SimpleEquationSyntaxAnalyser
+    {
+        private static readonly char[] digits1 = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static readonly char[] digits2 = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static readonly char[] operators = new[] { '+', '-', '/', '*' };
+
+        private Func<char, bool> digit1predicate = (char symbol) => digits1.Contains(symbol);
+        private Func<char, bool> digit2predicate = (char symbol) => digits2.Contains(symbol);
+        private Func<char, bool> operatorpredicate = (char symbol) => operators.Contains(symbol);
+
+        private int currentIndex = 0;
+
+        private bool expression(string input)
+        {
+            while (currentIndex < input.Length)
+            {
+                if (input.ElementAtOrDefault(currentIndex) == '-')
+                {
+                    this.currentIndex++;
+                    if (this.number(input))
+                    {
+                        while (!this.EmptySymbol(input))
+                        {
+                            if (this.operatorpredicate(input.ElementAtOrDefault(currentIndex)))
+                            {
+                                this.currentIndex++;
+                                if (this.number(input))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (input.ElementAtOrDefault(currentIndex) == '0')
+                {
+                    this.currentIndex++;
+
+                    while (!this.EmptySymbol(input))
+                    {
+                        if (this.operatorpredicate(input.ElementAtOrDefault(currentIndex)))
+                        {
+                            this.currentIndex++;
+
+                            if (this.number(input))
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    if (this.number(input))
+                    {
+                        while (!this.EmptySymbol(input))
+                        {
+                            if (this.operatorpredicate(input.ElementAtOrDefault(currentIndex)))
+                            {
+                                this.currentIndex++;
+                                if (this.number(input))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+
+            }
+            return true;
+
+        }
+
+        private bool number(string input)
+        {
+
+            if (this.digit1predicate(input.ElementAtOrDefault(currentIndex)))
+            {
+                currentIndex++;
+                if (this.numberPrime(input))
+                {
+                    return true;
+                }
+
+            }
+            else if (input.ElementAtOrDefault(currentIndex) == '0')
+            {
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        private bool numberPrime(string input)
+        {
+            while (currentIndex < input.Length)
+            {
+                if (this.digit2predicate(input.ElementAtOrDefault(currentIndex)))
+                {
+                    currentIndex++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return this.EmptySymbol(input) ? true : throw new InvalidOperationException("Something went wrong shoudln't hit this");
+        }
+
+        private bool EmptySymbol(string input)
+        {
+            if (currentIndex >= input.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public char[] TerminalSymbols => digits1.Concat(digits2).Concat(operators).ToArray();
+
+        public bool Validate(string input)
+        {
+            var trimmedInput = new string(input.Where(x => !string.IsNullOrWhiteSpace(x.ToString())).ToArray());
+            if (!trimmedInput.All(x => this.TerminalSymbols.Contains(x)))
+            {
+                return false;
+            }
+
+            var result = this.expression(trimmedInput);
+            this.currentIndex = 0;
+            return result;
+        }
+    }
+}
